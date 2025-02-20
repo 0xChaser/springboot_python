4adf430dc541fc03eb20f844a716b01ccbb29fee
chaser <florian.isak@icloud.com>
2025-02-20 18:40:59 +0100

    Apply new function

--- src/springboot_python/api/v1/__init__.py
+++ src/springboot_python/api/v1/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from springboot_python.api.v1.student import router as StudentRouter
 from springboot_python.api.v1.book import router as BookRouter
-
+from springboot_python.functions.route_require_admin import route_require_token
 
 __all__ = (
     StudentRouter,
@@ -14,4 +14,4 @@ router = APIRouter()
 
 
 for api_router in __all__:
-    router.include_router(api_router)
+    router.include_router(route_require_token(api_router))
